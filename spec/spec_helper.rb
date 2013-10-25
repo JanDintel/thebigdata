48e74d65d35f17c7f6db6dacaa36c1c5b4e60ad5
Jan van der Pas <janvanderpas@gmail.com>
2013-10-25 21:29:57 +0200

    Add Bundler to load path of specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,10 @@
+# Requires all the ruby config files within the /config directory
+Dir[File.join('./config/*.rb')].each {|f| require f}
 
-require 'rspec/core'
-require 'rspec/autorun'
+# require 'rspec/core'
+# require 'rspec/autorun'
 
-# Requires all ruby files within the ./lib/ direcorty and its subdirectories.
+# Requires all the ruby files within the /lib directory and its subdirectories
 Dir[File.join('./lib/**/*.rb')].each {|f| require f}
 
 RSpec.configure do |config|
