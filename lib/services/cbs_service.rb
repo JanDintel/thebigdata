c24da5ba1aeac2aae8a31a5c05dc1dde6bbf11c6
Jan van der Pas <janvanderpas@gmail.com>
2013-10-26 03:44:16 +0200

    Add CBSService class, to make and build API calls to the API

--- lib/services/cbs_service.rb
+++ lib/services/cbs_service.rb
@@ -1,7 +1,21 @@
+require 'ruby_odata'
+
 class CBSService
+  CBS_API = "http://opendata.cbs.nl/OData3StatlineCatalogService/Catalog/"
 
   def initialize(category)
     @category = category
   end
 
+  def new_collection_query
+    service = OData::Service.new(CBS_API)
+    service.Tables
+  end
+
+  def build_collection_query
+    new_collection_query.query
+  end
+
+  def execute_collection_query
+  end
 end
--- spec/services/cbs_service_spec.rb
+++ spec/services/cbs_service_spec.rb
@@ -7,4 +7,17 @@ describe CBSService do
     expect(cbs_service).to be_an_instance_of(CBSService)
   end
 
+  describe '#new_collection_query' do
+
+    it 'initializes a OData::QueryBuilder' do
+      expect(cbs_service.new_collection_query).to be_a(OData::QueryBuilder)
+    end
+  end
+
+  describe '#build_collection_query' do
+
+    it 'returns the scoped Tables query' do
+      expect(cbs_service.build_collection_query).to eql('/Tables')
+    end
+  end
 end
